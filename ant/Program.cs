d04648386914865b45bc2f46bb5b09ff6fbbabcc
jhoancompai <131424403+jhoancompai@users.noreply.github.com>
2024-05-17 16:19:27 -0400

    Add files via upload

---
+++ ant/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        string ants = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t";
+        int deadAnts = DeadAntCount(ants);
+        Console.WriteLine("Número de hormigas muertas: " + deadAnts);
+    }
+
+    public static int DeadAntCount(string antString)
+    {
+        if (string.IsNullOrEmpty(antString))
+        {
+            return 0;
+        }
+
+        antString = antString.Replace("ant", "");
+
+        int aCount = antString.Count(c => c == 'a');
+        int nCount = antString.Count(c => c == 'n');
+        int tCount = antString.Count(c => c == 't');
+
+        return Math.Max(Math.Max(aCount, nCount), tCount);
+    }
+}
+
